d958057942f9aedecb8e30503ff8213aa027d411
developkosarev <develop.kosarev@gmail.com>
2023-04-04 20:09:46 +0200

    Add message

--- app/code/Diko/OrderAttributes/Controller/Index/Team.php
+++ app/code/Diko/OrderAttributes/Controller/Index/Team.php
@@ -6,18 +6,25 @@ namespace Diko\OrderAttributes\Controller\Index;
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\View\Result\Page;
 use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\Message\ManagerInterface;
 
 class Team implements HttpGetActionInterface
 {
     private PageFactory $pageFactory;
 
-    public function __construct(PageFactory $pageFactory)
+    private ManagerInterface $messageManager;
+
+    public function __construct(PageFactory $pageFactory, ManagerInterface $messageManager)
     {
         $this->pageFactory = $pageFactory;
+        $this->messageManager = $messageManager;
     }
 
     public function execute(): Page
     {
+        $this->messageManager->addSuccessMessage(__('The record has been deleted.'));
+        $this->messageManager->addErrorMessage(__('The record does not exist.'));
+
         return $this->pageFactory->create();
     }
 }
