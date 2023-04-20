2c523752638f66a9c09ff176414f4865604e6d4f
developkosarev <develop.kosarev@gmail.com>
2023-04-20 18:31:36 +0200

    Add module

---
+++ app/code/Diko/Products/etc/Api/Data/ProductInterface.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Diko\Products\etc\Api\Data;
+
+interface ProductInterface
+{
+    //<item name="Magento\Catalog\Api\Data\ProductInterface" xsi:type="array">
+}
