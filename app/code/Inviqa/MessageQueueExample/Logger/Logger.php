3644f2d76f3fc1afd6fdc5d9701e3284d08b209c
developkosarev <develop.kosarev@gmail.com>
2023-05-29 19:48:37 +0200

    Add loger

---
+++ app/code/Inviqa/MessageQueueExample/Logger/Logger.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Inviqa\MessageQueueExample\Logger;
+
+use Monolog\Logger as BaseLogger;
+
+class Logger extends BaseLogger
+{
+}
--- app/code/Inviqa/MessageQueueExample/Model/Subscriber.php
+++ app/code/Inviqa/MessageQueueExample/Model/Subscriber.php
@@ -2,17 +2,26 @@
 
 namespace Inviqa\MessageQueueExample\Model;
 
-use Magento\Framework\Model\AbstractModel;
+use Inviqa\MessageQueueExample\Logger\Logger;
 use Inviqa\MessageQueueExample\Api\MessageInterface;
 use Inviqa\MessageQueueExample\Api\SubscriberInterface;
 
 class Subscriber implements SubscriberInterface
 {
+    private Logger $logger;
+
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function processMessage(MessageInterface $message)
     {
         echo 'Message received: ' . $message->getMessage() . PHP_EOL;
+
+        $this->logger->info($message->getMessage());
     }
 }
