48d16dd3306bc0a979a7a9236a0a7b21c724fb9f
developkosarev <develop.kosarev@gmail.com>
2023-07-11 10:38:22 +0200

    Add style

--- app/code/Diko/Newsletter/Block/Adminhtml/Edit/Tab/Newsletter.php
+++ app/code/Diko/Newsletter/Block/Adminhtml/Edit/Tab/Newsletter.php
@@ -8,4 +8,33 @@ use Magento\Customer\Block\Adminhtml\Edit\Tab\Newsletter as BaseNewsletter;
 class Newsletter extends BaseNewsletter
 {
     protected $_template = 'Diko_Newsletter::tab/newsletter.phtml';
+
+    public function initForm()
+    {
+        parent::initForm();
+
+        $websiteId = 1;
+
+        $fieldset = $this->_form->addFieldset(
+            'base_fieldset_1',
+            [
+                'legend' => __('Newsletter Information2'),
+                'class' => 'customer-newsletter-fieldset',
+            ]
+        );
+
+        $fieldset->addField(
+            'subscription_test_' . $websiteId,
+            'select',
+            [
+                'label' => __('Subscribed on Store View'),
+                'name' => "subscription_test[$websiteId]",
+                'data-form-part' => $this->getData('target_form'),
+                'values' => [1 => 'first', '2' => 'second'],
+                'value' => 2,
+            ]
+        );
+
+        return $this;
+    }
 }
---
+++ app/code/Diko/Newsletter/view/adminhtml/web/js/vue-test.js
@@ -0,0 +1,9 @@
+define(['jquery'], function($) {
+    'use strict';
+
+    console.log('Diko vue test');
+
+    return function(config, element) {
+        console.log('Diko vue test');
+    }
+});
