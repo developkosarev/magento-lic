580f6345d3c09a2ea42357b58228f5b11d40f547
developkosarev <develop.kosarev@gmail.com>
2024-01-01 14:36:05 +0100

    Add magepack

---
+++ app/code/Diko/MagePack/Block/BundlesLoader.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Diko\MagePack\Block;
+
+use MageSuite\Magepack\Block\BundlesLoader as Base;
+
+class BundlesLoader extends Base
+{
+    protected $_template = 'Diko_MagePack::bundles-loader.phtml';
+}
